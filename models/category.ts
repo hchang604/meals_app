3adff52b65a4b10be194660fc89c821838ba49a0
hchang604 <henry.chang@alianza.com>
2022-03-10 21:46:29 -0800

    add dummy data for stack navigation

---
+++ models/category.ts
@@ -0,0 +1,13 @@
+class Category {
+  id;
+  title;
+  color;
+
+  constructor(id: string, title: string, color: string) {
+    this.id = id;
+    this.title = title;
+    this.color = color;
+  }
+}
+
+export default Category;
